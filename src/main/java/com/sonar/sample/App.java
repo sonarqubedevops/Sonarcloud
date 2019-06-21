2105bf022cd1262d9d6383ebbbe0ee644b138a50
s <s@gmail.com>
2019-06-21 13:24:28 +0530

    done

---
+++ src/main/java/com/sonar/sample/App.java
@@ -0,0 +1,15 @@
+package com.sonar.sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+        int firstnum;
+        int secondnum;
+        
+        int adder (int firstnum,int secondnum) {
+        	return firstnum + secondnum;
+        }
+}
