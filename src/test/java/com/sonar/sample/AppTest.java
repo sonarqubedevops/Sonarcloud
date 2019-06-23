0d623a15262c8906c5ff281bbfa78cd9f824f228
s <s@gmail.com>
2019-06-23 18:00:07 +0530

    added

--- src/test/java/com/sonar/sample/AppTest.java
+++ src/test/java/com/sonar/sample/AppTest.java
@@ -1,38 +1,17 @@
 package com.sonar.sample;
-
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
+    App obj = new App();
+    
+    @Test
     public void testApp()
     {
-        assertTrue( true );
+        assertEquals(10,obj.adder(4,6));
     }
 }
